542f2fe6b9ea157be30229b28eb97c6db32f2d68
BBlanthos <159204109+BBlanthos@users.noreply.github.com>
2025-12-17 14:25:05 -0500

    Create rank.lua
    i love committing untested code

---
+++ items/rank.lua
@@ -0,0 +1,21 @@
+SMODS.Rank {
+	hc_atlas = 'atlas_placeholder.png',
+    lc_atlas = 'atlas_placeholder.png',
+	
+
+    key = '2.7',
+    card_key = '2.7',
+    nominal = 2.7,
+
+strength_effect = {
+            fixed = 3,
+            random = false,
+            ignore = false
+        },
+
+	
+ next = { '3' },
+prev= { '2' },
+	
+    shorthand = '2.7'
+}
